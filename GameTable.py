fcbb815772bfff4dbcd12c8b18cc92355e06a15a
persona5guy <65686253+persona5guy@users.noreply.gitub.com>
2020-06-11 10:12:08 +1200

    Start of 11/6/2020

--- GameTable.py
+++ GameTable.py
@@ -5,7 +5,20 @@
 #------------------------------#
 #============Changes===========#
 ## Change date: 10/6/2020
-## Change Branch: Master
-# Formatting of the code information
+## Change Branch: Collection
+# Created basic class
 
 #=======Dummy Variables========#
+
+#==========Game Class==========#
+
+class Game:
+    def __init__(self, game_name, _total_hours, played_hours, priority):
+        self.game_name = game_name
+        self._total_hours = _total_hours
+        self.played_hours = played_hours
+        self.priority = priority
+
+    def remaining_hours(self, _total_hours, played_hours):
+        return(_total_hours - played_hours)
+        
